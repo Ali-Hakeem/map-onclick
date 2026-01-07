dd98231ea13ef6e6595881893f78b13ba1d77bda
unknown <Ali-Hakeem@github.com>
2026-01-07 21:44:03 +0700

    first

--- script.js
+++ script.js
@@ -37,14 +37,22 @@ document.addEventListener("DOMContentLoaded", () => {
       <h4><span class="badge bg-danger">${data.title}</span></h4>
       <p class="fw-bold"><i class="fa-solid fa-map-location-dot me-2 text-orange"></i> ${data.alamat}</p>
 
-      <div class="mt-2 d-flex flex-wrap gap-3">
-        <!-- pemilik -->
-        <div class="px-3 py-3 border rounded justify-content-between align-items-center w-100">
-          <p class="mb-0 text-secondary">PEMILIK</p>
-          <i class="fa-solid fa-person text-info"></i> <span class="fw-bold text-dark mx-1">${data.pemilik}</span>
-        </div>
-      </div>
+      // <div class="mt-2 d-flex flex-wrap gap-3">
+      //   <!-- pemilik -->
+      //   <div class="px-3 py-3 border rounded justify-content-between align-items-center w-100">
+      //     <p class="mb-0 text-secondary">PEMILIK</p>
+      //     <i class="fa-solid fa-person text-info"></i> <span class="fw-bold text-dark mx-1">${data.pemilik}</span>
+      //   </div>
+      // </div>
       <div class="row g-3 mt-1">
+        <!-- PEMILIK -->
+        <div class="col-md-4 col-12">
+          <div class="px-3 py-3 border rounded h-100">
+            <p class="mb-0 text-secondary">PEMILIK</p>
+            <i class="fa-regular fa-newspaper text-warning"></i> <span class="fw-bold text-dark mx-1" style="font-size: 14px;">${data.pemilik}</span>
+          </div>
+        </div>
+
         <!-- STATUS -->
         <div class="col-md-4 col-12">
           <div class="px-3 py-3 border rounded h-100">
@@ -52,7 +60,9 @@ document.addEventListener("DOMContentLoaded", () => {
             <i class="fa-regular fa-newspaper text-warning"></i> <span class="fw-bold text-dark mx-1" style="font-size: 14px;">${data.status}</span>
           </div>
         </div>
-
+      </div>  
+      
+      <div class="row g-3 mt-1">
         <!-- SPPT -->
         <div class="col-md-4 col-12">
           <div class="px-3 py-3 border rounded h-100">
